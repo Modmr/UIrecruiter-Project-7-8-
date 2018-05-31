25706ac55e8062e50165edac1435fd4f2f0a6ce4
Modmr <mo_42@hotmail.nl>
2018-05-31 12:56:28 +0200

    color change

--- recruiter/src/components/Home.tsx
+++ recruiter/src/components/Home.tsx
@@ -8,8 +8,13 @@ export class Home extends React.Component<any, any>{
 
   public render() {
     return (
-        <div style={{height: 1500, marginTop: "50px"}}>
-          <p>test</p>
+        <div style={{height: 1500, marginTop: "40px", marginLeft:-10}}>
+          
+          <iframe
+          width="101%"
+          height="40%"
+          src="https://console.dialogflow.com/api-client/demo/embedded/d311c341-031c-413f-be25-47f1d828771a"/>
+
         </div>
     );
   }
