d585ddd7dcb6cc13e2e273a8d9f3939cd079e83c
endyhu <endyhu99@gmail.com>
2018-06-17 16:16:57 +0200

    Increase rate

--- python-recruiter/audio.py
+++ python-recruiter/audio.py
@@ -7,7 +7,7 @@ def record(subject_input):
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
-    RATE = 16000
+    RATE = 44100
     RECORD_SECONDS = 4
     WAVE_OUTPUT_FILENAME = subject_input
 
